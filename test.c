05330ceddc3d254cf8df48ba120080ebde3dd207
Stefan Mijalkov <stefanmijalkov1997@gmail.com>
2021-04-29 13:39:07 -0400

    Testing

--- test.c
+++ test.c
@@ -4,6 +4,6 @@ int main(){
     int x,y;
     printf("Enter 2 numbers separated by space\n");
     scanf("%d %d", &x,&y);
-    printf("Result is %d", x+y);
+    printf("Result is %d\n", x+y);
     return 0;
 }
